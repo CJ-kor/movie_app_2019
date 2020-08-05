e63454501222438a5925b8f712051761a86afb43
CJ KIM <zzalzzary@gmail.com>
2020-08-05 18:55:06 +0900

    Updates

---
+++ precache-manifest.5a484e2a0076b25ef7f5995bf23b9f78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7628b68c4bfc17eb8b364177949da2a",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e2e32225452b9ad518a8",
+    "url": "/movie_app_2019/static/css/main.5721af45.chunk.css"
+  },
+  {
+    "revision": "fde87d2ad6cabe52f608",
+    "url": "/movie_app_2019/static/js/2.c3d73534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.c3d73534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2e32225452b9ad518a8",
+    "url": "/movie_app_2019/static/js/main.50627ef3.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
